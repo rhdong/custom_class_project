9be9cd159f4d149302cf119e42e870b81a006f2b
rhdong <rhdong2017@gmail.com>
2022-10-18 08:04:35 +0800

    [Fix] compile error on some old arch GPU

--- run_build.sh
+++ run_build.sh
@@ -4,6 +4,6 @@ rm -rf build;
 mkdir build;
 cd build
 
-cmake -DCMAKE_PREFIX_PATH="$(python -c 'import torch; print(torch.__path__[0])')" ..
+cmake  -DCMAKE_BUILD_TYPE=Release -Dsm=80 -DCMAKE_PREFIX_PATH="$(python -c 'import torch; print(torch.__path__[0])')" ..
 
 make -j
